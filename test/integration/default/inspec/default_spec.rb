350fbb28d4ba99f82b49833924f63f3e180b476d
GreetDB <grdbaets@gmail.com>
2017-07-10 16:05:25 +0100

    update R version

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -31,7 +31,7 @@ end
 
 describe command('R --version') do
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(/R version 3.4.0/) }
+  its('stdout') { should match(/R version 3.4.1/) }
 end
 
 describe command('Rscript -e \'packageVersion("cogena")\'') do
